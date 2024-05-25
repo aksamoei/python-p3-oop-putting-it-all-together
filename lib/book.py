d1467aaeb2d9b3ad5252c3812b11a5e4f6939ca0
Abraham <abukip344@gmail.com>
2024-05-25 07:35:21 -0400

    putting it together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    '''blueprints a book'''
+    def __init__(self,title,page_count):
+        '''initialie attributes'''
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        '''age_count property'''
+        return self._page_count
     
+    @page_count.setter
+    def page_count(self, page_count):
+        '''set page_count'''
+        if type(page_count) in (int, float):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+            
+    #page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        '''turns book page'''
+        print("Flipping the page...wow, you read fast!")
+
+
+
+kid_book = Book("Aman", 20)
+
+print(kid_book.turn_page())
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    '''blueprints shoe'''
+    def __init__(self, brand, size):
+        '''initialize attributes'''
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        '''shoe size property'''
+        return self._size 
+    
+    @size.setter
+    def size(self, size):
+        '''set size with validation'''
+        if type(size) not in (int, float):
+            print("size must be an integer")
+        else:
+            self._size = size
+        
+    #size = property(get_size, set_size)
+
+    def cobble(self):
+        '''repais shoe'''
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+youth_shoe = Shoe("Adibas", 7)
+
+print(youth_shoe.cobble())
+print(youth_shoe.condition)
